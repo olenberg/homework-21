efdc355362455b49761eb573ecf444452ca24115
Artur Olenberg <olenberg.ai@gmail.com>
2023-02-12 18:07:51 +0600

    refactor

--- classes/request.py
+++ classes/request.py
@@ -7,17 +7,17 @@ class Request:
         self._product: str = data[2]
 
     @property
-    def location_from(self):
+    def location_from(self) -> str:
         return self._location_from
 
     @property
-    def location_to(self):
+    def location_to(self) -> str:
         return self._location_to
 
     @property
-    def amount(self):
+    def amount(self) -> int:
         return self._amount
 
     @property
-    def product(self):
+    def product(self) -> str:
         return self._product
